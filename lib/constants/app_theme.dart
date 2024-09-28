68179f76560fc083fb34bb349062804d5f3c8ab0
nada <nadaelassal00@icloud.com>
2024-09-28 23:13:47 +0300

    apptheme

--- lib/constants/app_theme.dart
+++ lib/constants/app_theme.dart
@@ -3,7 +3,10 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Color(0XFF39A552);
+  static const Color primary = Color(0XFF707070);
+  static const Color gold = Color(0XFFFFB224);
+  static const Color white = Color(0XFFFFFFFF);
+  static const Color grey = Color(0XFFC6C6C6);
 
   static ThemeData lightTheme = ThemeData();
 }
